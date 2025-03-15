c1a75d251b70ba616c58b0f2cb5379b10ef23f3f
Ranit Parida <ranit0120@gmail.com>
2025-03-15 05:18:58 +0000

    By Ranit

---
+++ 169. Majority Elements/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for(int x:nums){
+            if(count == 0){
+                candidate = x;
+                count = 1;
+            }else{
+                if(candidate == x){
+                    count++;
+                }else{
+                    count--;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
